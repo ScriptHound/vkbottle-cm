feac8c43a2c105140e28578dd869bbe30e0df880
ScriptHound <grinkiko@gmail.com>
2021-05-07 20:46:39 +0300

    squashed migrations
    added conversation model

--- src/admin/models/user.py
+++ src/admin/models/user.py
@@ -1,5 +1,7 @@
-from sqlalchemy import Column, Integer, String, Boolean, Date, Text
-
+from sqlalchemy import (
+    Column, Integer, String,
+    Boolean, Date, Text, ForeignKey)
+from sqlalchemy.orm import relationship
 from src.admin.models.base import Base
 
 
@@ -12,3 +14,14 @@ class User(Base):
     is_admin = Column(Boolean, default=False)
     is_whitelisted = Column(Boolean, default=False)
     join_date = Column(Date)
+
+    conversation_id = Column(Integer, ForeignKey('conversation.id'))
+
+
+class Conversation(Base):
+    __tablename__ = 'conversation'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    participants_number = Column(Integer)
+
+    users = relationship('User')
