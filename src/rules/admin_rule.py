ada8d126e52b29ca0c8c3987e5b01735bdc7f722
FeeeeK <krmrclemens@gmail.com>
2021-05-09 02:26:18 +0300

    Added rule for checking admin permissions

---
+++ src/rules/admin_rule.py
@@ -0,0 +1,26 @@
+from vkbottle.bot import rules, Message
+from typing import Union
+
+
+class AdminRule(rules.ABCMessageRule):
+    def __init__(self, return_admins: bool = False):
+        self.return_admins = return_admins
+
+    async def check(self, message: Message) -> Union[dict, bool]:
+        items = (
+            await message.ctx_api.messages.get_conversations_by_id(peer_ids=message.peer_id)
+        ).items
+
+        if items:
+            chat_settings = items[0].chat_settings
+            admins = [chat_settings.owner_id, *chat_settings.admin_ids]
+        else:
+            return False
+
+        if message.from_id not in admins:
+            await message.answer("У вас нет доступа к этой команде.")
+            return False
+
+        if self.return_admins:
+            return {"admins": admins}
+        return True
